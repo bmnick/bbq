a8e3bdbdbb01d4e73613e12241f35d914120aff9
Ben Nicholas <ben@ben-nicholas.com>
2012-02-02 20:42:27 -0500

    initial project with testing frameworks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbq::Application.config.secret_token = 'c13639ca3ac2cf01cb470f10e68f7a657cb8851cb196c8169819a2485834bf3fe4c8b64f89c9ded259a0e6b37db093818d660067e0b5ca3c1278b7e459158a2a'
