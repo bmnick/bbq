db76d1b870b2dbab5a52089b5e68dad18f068aff
Ben Nicholas <ben@ben-nicholas.com>
2012-02-05 01:48:43 -0500

    Add create method, and nicer response type handling

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,20 @@
 class QuotesController < ApplicationController
 	before_filter :find_quote
 
+	respond_to :html, :json
+
 	def new
-		
+	end
+
+	def create
+		@quote.save
+		respond_with @quote
 	end
 
 	private
 	def find_quote
 		@quote = Quote.find(params[:id]) if params[:id]
+		@quote ||= Quote.new(params[:quote]) if params[:quote]
 		@quote ||= Quote.new
 	end
 end
