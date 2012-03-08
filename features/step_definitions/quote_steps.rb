db053d961ca70504ae036cb368c8ff7150524174
Ben Nicholas <ben@ben-nicholas.com>
2012-03-07 22:02:53 -0500

    Add score step to create quote helper

--- features/step_definitions/quote_steps.rb
+++ features/step_definitions/quote_steps.rb
@@ -6,6 +6,7 @@ Given /^the following quote exists:$/ do |table|
   q.text = table.rows_hash['Text']
   q.context = table.rows_hash["Context"]
   q.id = table.rows_hash['Number'].to_i if table.rows_hash.key?('Number')
+  q.score = table.rows_hash['Score'].to_i if table.rows_hash.key?('Score')
 
   q.save!
 end
