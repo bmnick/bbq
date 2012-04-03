38419f8ff99948a2e4bc95c1f7a9ea0775601071
Ben Nicholas <ben@ben-nicholas.com>
2012-04-03 16:35:21 -0400

    Add search ability to tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,10 +2,15 @@ class TagsController < ApplicationController
   before_filter :find_tags, only: [:index]
   
   def index
+    respond_to do |format|
+      format.html 
+      format.json { render json: @tags }
+    end
   end
   
   protected
   def find_tags
-    @tags = Tag.all
+    @tags = Tag.where("name like ?", "%#{params[:q]}%")
+    @tags ||= Tag.all
   end
 end
